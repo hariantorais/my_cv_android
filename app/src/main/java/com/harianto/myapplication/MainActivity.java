8717929f91694a53250ebf1a83cc9bbe4fd9ffd9
HariantoRais <ibnurais.abdillah@gmail.com>
2022-12-16 22:41:21 +0800

    penambahan webview

--- app/src/main/java/com/harianto/myapplication/MainActivity.java
+++ app/src/main/java/com/harianto/myapplication/MainActivity.java
@@ -2,13 +2,30 @@ package com.harianto.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button btnHubungi;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnHubungi = findViewById(R.id.btn_hubungi);
+
+        String telepon = "tel:081935392412";
+
+        btnHubungi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent call = new Intent(Intent.ACTION_DIAL, Uri.parse(telepon));
+                startActivity(call);
+            }
+        });
     }
 }
